e83c2740083400b7ab9c0e724987610ef21f040b
Guy Golan <guygolan91@gmail.com>
2022-05-24 12:25:11 +0300

    first

---
+++ src/schemaBuilder.ts
@@ -0,0 +1,55 @@
+type SchemaBuilderOptions = {
+	baseUrl: string;
+};
+
+type BuildFn = (searchParams?: SearchParams) => string;
+type Builder<Param, Return> = (p: Param) => Return;
+export type WithBuild<T> = T & { build: BuildFn };
+
+export type SearchParams = { [key: string]: string | undefined };
+
+export const schemaBuilder = function ({ baseUrl }: SchemaBuilderOptions) {
+	let urlPath = [baseUrl];
+
+	const build: BuildFn = (searchParams?: SearchParams) => {
+
+		if (!searchParams) return urlPath.join('/');
+
+		function buildSearchParams(params: SearchParams) {
+			return Object
+				.entries(params)
+				.filter(([, val]) => Boolean(val))
+				.map(([key, val]) => `${key}=${val}`)
+				.join('&');
+		}
+
+		return `${urlPath.join('/')}?${buildSearchParams(searchParams)}`;
+	};
+
+	function path<Sub>(name: string, next: () => Sub): Builder<void, Sub> {
+		return () => {
+			urlPath.push(name);
+			return { ...next() };
+		};
+	}
+
+	function param<P extends string, Sub>(next: () => Sub): Builder<P, Sub> {
+		return (pathParam: P) => {
+			urlPath.push(pathParam);
+			return { ...next() };
+		};
+	}
+
+	function endpoint<T>(sub: T): () => WithBuild<T> {
+		return () => ({
+			...sub,
+			build,
+		});
+	}
+
+	return {
+		endpoint,
+		param,
+		path,
+	};
+};
